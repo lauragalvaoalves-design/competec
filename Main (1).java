5ecfabad7b5cdd000c8f714fe5ee2e3a4cdd3a15
Laura Alves Galvão <lauragalvaoalves@icloud.com>
2026-05-09 10:58:32 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    /*int 1 = abacaxi;
+	    int 2 = goiaba;
+	    int 3 = laranja;*/
+	    int codigo = sc.nextInt();
+
+	    if  (codigo == 1){
+	  System.out.println("abacaxi");
+	    }
+	       if  (codigo == 2){
+	  System.out.println("goiaba");
+	    }
+	       if  (codigo == 3){
+	  System.out.println("laranja");
+	    }
+	}
+}
