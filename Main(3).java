11f7db890278245ce9c3334d1f664881f85878e6
Laura Alves Galvão <lauragalvaoalves@icloud.com>
2026-06-13 11:27:19 -0300

    Add files via upload

---
+++ Main(3).java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arranjo [] = {1,4,7};
+		int soma = arranjo[0]+arrranjo[1]+arrranjo[2];
+		
+	    System.out.println(soma);
+	    
+	}	
+}
